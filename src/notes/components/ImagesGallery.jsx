9f8520f24b002d73100980f6daaa4ebc6225b978
Juanfelipegrc <felipegarciarjs@gmail.com>
2024-07-12 21:57:01 -0500

    Added Video Settings

--- src/notes/components/ImagesGallery.jsx
+++ src/notes/components/ImagesGallery.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 export const ImagesGallery = ({images}) => {
+
+
+  
   return (
     <>
-    
+
         <div style={{overflowY: images.length < 2? 'hidden' : 'scroll'}} className='container-images-gallery-component'>
 
 
             {
                 images.map(image => (
                     <div key={image} className='item-images-gallery'>
-                        <img className='image-images-gallery' src={image}/>
+                        {
+                            image.includes('.webp') || image.includes('.png') ||
+                            image.includes('.jpg') || image.includes('.jpeg') ||
+                            image.includes('.gif') || image.includes('.svg')
+                            ?
+                            <img className='image-images-gallery' src={image}/>
+                            :
+                            <video className='image-images-gallery' src={image} controls/>
+
+
+                        }
+                        
                     </div>
                 ))
             }
